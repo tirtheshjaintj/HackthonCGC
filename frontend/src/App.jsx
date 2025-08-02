adbf4bde2bec4ce8fa1ab754eddfd897f69e6da4
Aniket Gupta <aniketgupta.org@gmail.com>
2025-08-02 14:12:57 +0530

    add auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './modules/auth/Register';
-import VerifyOtp from './modules/auth/VerifyOtp';
-import Login from './modules/auth/Login';
-import AdminLogin from './modules/adminAuth/Login';
-import HomePage from './modules/home/pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./modules/auth/Login";
+import AdminLogin from "./modules/adminAuth/Login";
+import HomePage from "./modules/home/pages/HomePage";
+import SignUp from "./modules/auth/SignUp";
 // import other components like Login, Home, etc. when available
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Register />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} /> */}
-
+        <Route path="/register" element={<SignUp />} />
+ 
         {/* Example routes */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
-        
-        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/" element={<HomePage />} />
 
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/admin/login" element={<AdminLogin />} />
       </Routes>
     </Router>
   );
